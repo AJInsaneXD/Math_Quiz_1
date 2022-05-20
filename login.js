4703225b2219a21cc5ec9e9b27ae3a73b7252f38
AJ Insane <86559435+Anshuman2883@users.noreply.github.com>
2022-05-20 05:35:24 -0800

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function Login(){
-    player_1_name = document.getElementById("player_1_input").value;
-    player_2_name = document.getElementById("player_2_input").value;
-
-    localStorage.setItem("player_1_name",player_1_name);
-    localStorage.setItem("player_2_name",player_2_name);
-
-    window.location = "main_quiz.html"
-}
